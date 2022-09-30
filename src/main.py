d6becacecc05f994058e3f95ae2f744b123ba037
carlaterboven <carla.terboven@student.hpi.de>
2022-09-30 15:48:41 +0200

    Add indicator for GPS measurements

--- src/main.py
+++ src/main.py
@@ -16,6 +16,12 @@ import write_data as data_logger
 if __name__ ==  '__main__':
     print("Read and collect data. [Press Ctrl+C to exit!]")
     GPIO.setmode(GPIO.BCM)
+    GPIO.setup(21, GPIO.OUT) # pin for LED to indicate successfull measurement
+    GPIO.output(21, GPIO.LOW)
+    
+    print("Please connect GPS sensor via USB.")
+    # wait 10 seconds to connect GPS sensor
+    time.sleep(10)
 
     #pmsensor_sps = pmsensor_sensirion.PMSensorSensirion()
     pmsensor_5003 = pmsensor_5003.PMSensor5003()
@@ -25,7 +31,8 @@ if __name__ ==  '__main__':
     gassensor = gassensor_cjmcu6814.GasSensor()
 
     gps = gps.Gps()
-    datalogger = data_logger.DataLogger('../data/log.csv')
+    file_name = 'log' + str(time.time()) + '.csv'
+    datalogger = data_logger.DataLogger('/home/pi/Dokumente/air_quality_sensing/data/' + file_name)
 
     try:
         while True:
@@ -34,15 +41,15 @@ if __name__ ==  '__main__':
             pmsensor_7003.reset_data()
             temp_hum_sensor.reset_data()
             gassensor.reset_data()
-            # collect data for 600 seconds (10minutes)
-            t_end = time.time() + 600
+            # collect data for 3 seconds
+            t_end = time.time() + 3
             while time.time() < t_end:
                 #pmsensor_sps.read_data()
                 pmsensor_5003.read_data()
                 pmsensor_7003.read_data()
                 temp_hum_sensor.read_data()
                 gassensor.read_data()
-                time.sleep(5) # measure every 5 seconds
+                time.sleep(1) # measure every second
             #print(pmsensor_sps.get_data())
             #print(pmsensor_5003.get_data())
             #print(pmsensor_7003.get_data())
@@ -50,6 +57,9 @@ if __name__ ==  '__main__':
             #print(gassensor.get_data())
             try:
                 gps.compute_position()
+                if gps.get_data()['lat'] != 'n/a':
+                    # use LED to show successfull GPS
+                    GPIO.output(21, GPIO.HIGH)
             except ConnectionResetError:
                 print('ConnectionResetError')
             #datalogger.write_data(gps.get_data() | pmsensor_sps.get_data() | pmsensor_5003.get_data() | pmsensor_7003.get_data() | temp_hum_sensor.get_data() | gassensor.get_data())
